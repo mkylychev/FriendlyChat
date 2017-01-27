c838461805dcdfb0bc48a71f25d624cf21abfc1d
mkylychev <mkylychev@gmail.com>
2017-01-27 14:02:03 +0600

    add Photo picker

--- app/src/main/java/com/example/mkylychev/frieandlychat/MainActivity.java
+++ app/src/main/java/com/example/mkylychev/frieandlychat/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.mkylychev.frieandlychat;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
@@ -27,6 +28,8 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -40,6 +43,7 @@ public class MainActivity extends AppCompatActivity {
     public static final String ANONYMOUS = "anonymous";
     public static final int DEFAULT_MSG_LENGTH_LIMIT = 1000;
     public static final int RC_SIGN_IN=1;
+    public static final int RC_PHOTO_PICKER=2;
 
     private ListView mMessageListView;
     private MessageAdapter mMessageAdapter;
@@ -55,6 +59,8 @@ public class MainActivity extends AppCompatActivity {
     private ChildEventListener mChildEventListener;
     private FirebaseAuth mFirebaseAuth;
     private FirebaseAuth.AuthStateListener mAuthStateListener;
+    private FirebaseStorage mFirebaseStorage;
+    private StorageReference mChatPhotosStorageReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -67,6 +73,7 @@ public class MainActivity extends AppCompatActivity {
         mFirebaseAuth=FirebaseAuth.getInstance();
 
         mMessagesDatabaseReference = mFirebaseDatabase.getReference().child("messages");
+        mChatPhotosStorageReference=mFirebaseStorage.getReference().child("chat_photos");
         // Initialize references to views
         mProgressBar = (ProgressBar) findViewById(R.id.progressBar);
         mMessageListView = (ListView) findViewById(R.id.messageListView);
@@ -86,7 +93,11 @@ public class MainActivity extends AppCompatActivity {
         mPhotoPickerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // TODO: Fire an intent to show an image picker
+                Intent intent=new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/jpeg");
+                intent.putExtra(Intent.EXTRA_LOCAL_ONLY,true);
+                startActivityForResult(Intent.createChooser(intent,"Complete action using"),
+                        RC_PHOTO_PICKER);
             }
         });
 
@@ -154,6 +165,10 @@ public class MainActivity extends AppCompatActivity {
             }else if(resultCode==RESULT_CANCELED){
                 Toast.makeText(this,"Sign in canceled",Toast.LENGTH_SHORT).show();
                 finish();
+            }else if(requestCode==RC_PHOTO_PICKER && requestCode==RESULT_OK){
+                Uri selectedImageUri=data.getData();
+                StorageReference photoRef=
+                        mChatPhotosStorageReference.child(selectedImageUri.getLastPathSegment());
             }
         }
     }
